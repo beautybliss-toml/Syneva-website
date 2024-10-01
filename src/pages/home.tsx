cef717141949b409d4ae401edb8efdef68e83608
beautybliss-toml <beautybliss0508@gmail.com>
2024-10-02 02:40:58 +0900

    add increasing number

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -10,22 +10,53 @@ import {
 } from "../components";
 
 const Home: React.FC = () => {
-  const [totalLockedFunds, setTotalLockedFunds] = useState<string>("");
-  const [totalUsers, setTotalUsers] = useState<string>("");
-  const [totalOperations, setTotalOperations] = useState<string>("");
+  const [totalLockedFunds, setTotalLockedFunds] = useState<number>(0);
+  const [totalUsers, setTotalUsers] = useState<number>(0);
+  const [totalOperations, setTotalOperations] = useState<number>(0);
+
+  const formatNumberWithPlus = (num: number): string => {
+    // Convert number to string with commas as thousand separators
+    const formattedNum = num.toLocaleString();
+    // Add a '+' at the end
+    return `${formattedNum}+`;
+  }
+
+  const incrementNumber = (totalLockedFunds: number, totalUsers: number, totalOperations: number, interval: number = 50) => {
+    let currentLockedFunds = 0;
+    let currentUsers = 0;
+    let currentOperations = 0;
+    
+    const step1 = Math.ceil(totalLockedFunds / 100); 
+    const step2 = Math.ceil(totalUsers / 100); 
+    const step3 = Math.ceil(totalOperations / 100);
+    const timer = setInterval(() => {
+        if (currentLockedFunds + step1 >= totalLockedFunds) {
+            clearInterval(timer); 
+            setTotalLockedFunds(totalLockedFunds);
+            setTotalUsers(totalUsers);
+            setTotalOperations(totalOperations)
+        } else {
+            currentLockedFunds += step1; 
+            currentUsers += step2;
+            currentOperations += step3;
+            setTotalLockedFunds(currentLockedFunds);
+            setTotalUsers(currentUsers);
+            setTotalOperations(currentOperations);
+        }
+    }, interval); 
+  }
 
   useEffect(() => {
     // Simulate data fetching from an API
     const fetchData = async () => {
       // Simulating API response
       const data = {
-        totalLockedFunds: "1,240,000+",
-        totalUsers: "2,400+",
-        totalOperations: "17,000+",
+        totalLockedFunds: 1240000,
+        totalUsers: 2400,
+        totalOperations: 17000,
       };
-      setTotalLockedFunds(data.totalLockedFunds);
-      setTotalUsers(data.totalUsers);
-      setTotalOperations(data.totalOperations);
+      
+      incrementNumber(data.totalLockedFunds, data.totalUsers, data.totalOperations, 15);
     };
 
     fetchData();
@@ -36,9 +67,9 @@ const Home: React.FC = () => {
       <HeroSection />
       <Advantages />
       <Numbers
-        totalLockedFunds={totalLockedFunds}
-        totalUsers={totalUsers}
-        totalOperations={totalOperations}
+        totalLockedFunds={formatNumberWithPlus(totalLockedFunds)}
+        totalUsers={formatNumberWithPlus(totalUsers)}
+        totalOperations={formatNumberWithPlus(totalOperations)}
       />
       <Partners />
       <Review />
