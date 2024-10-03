6d879c78c409b6a69d0e384e316d615d10194d5c
beautybliss-toml <beautybliss0508@gmail.com>
2024-10-03 09:37:36 +0900

    add gsap

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,4 +2,4 @@ export { default as Home } from "./home";
 export { default as Team } from "./team";
 export { default as Stake } from "./stake";
 export { default as Swap } from "./swap";
-export { default as Privacy } from "./Privacy";
+export { default as Privacy } from "./privacy";
