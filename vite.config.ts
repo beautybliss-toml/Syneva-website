c1cca5acffd7b5737bdecf4083f714690648f4f0
beautybliss-toml <beautybliss0508@gmail.com>
2024-11-07 22:31:58 +0100

    fix issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+// import rollupNodePolyfills from 'rollup-plugin-node-polyfills';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    esbuildOptions: {
+      // Polyfill Node.js global to access Buffer
+      define: {
+        global: 'globalThis'
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true
+        })
+      ]
+    }
+  },
+  build: {
+    // rollupOptions: {
+    //   plugins: [
+    //     rollupNodePolyfills()  // Use imported polyfill plugin here
+    //   ]
+    // }
+  }
+});
